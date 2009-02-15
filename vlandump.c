03f0f3dc8622eb7738909a4bf60ce0aa16cfb6d7
Pascal Mainini <pascal@mainini.ch>
2009-02-15 00:21:24 +0000

    rewritten to use array

--- vlandump.c
+++ vlandump.c
@@ -29,21 +29,22 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include <stdint.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <pcap.h>
 
-#define VERSION "1.0"
-#define ALLOC_COUNT 10
+#define VERSION "2.0"
+#define NUM_VLANS 4096
 #define PRINTHELP() printf("Usage: vlandump [-h] [-v] [-o] [-c] "\
                            "[-r file] [-i iface]\n")
 
-///////////////////////////// structures
 
-typedef struct
-{
+///////////////////////////// types
+
+typedef uint64_t count_t;
+
+typedef struct {
     uint8_t destination[6];
     uint8_t source[6];
     uint16_t type;
@@ -51,27 +52,23 @@ typedef struct
 #define ET_VLAN 0x0081
 #define SIZE_ETHERNET sizeof(header_ethernet_t)
 
-typedef struct
-{
-    uint16_t vlan_id;
-    uint64_t count;
-} vlan_count_t;
+typedef struct {
+    count_t count;
+    count_t id;
+} vlan_entry_t;
 
 
 ///////////////////////////// global vars
 
-uint8_t verbose = 0;
-uint8_t sort_by_count = 0;
-uint8_t reverse_order = 0;
+char verbose = 0;
+char sort_by_count = 0;
+char reverse_order = 0;
 pcap_t *handle;
 
-vlan_count_t *counts = NULL;
-uint16_t num_of_allocated_counts = 0;
-uint16_t highest_used_count = 0;
-
-uint64_t total_count = 0;
-uint64_t tagged_count = 0;
-uint64_t untagged_count = 0;
+count_t vlans[NUM_VLANS][2];
+count_t total_count = 0;
+count_t tagged_count = 0;
+count_t untagged_count = 0;
 
 
 ///////////////////////////// functions
@@ -80,61 +77,24 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *p)
 {   
     total_count++;
 
-    if(header->len < SIZE_ETHERNET)
-    {
+    if(header->len < SIZE_ETHERNET) {
         return;
     }
 
     const header_ethernet_t *ethernet = (header_ethernet_t*)(p);
 
-    if(ethernet->type == ET_VLAN)
-    {   
+    if(ethernet->type == ET_VLAN) {
         tagged_count++;
-        uint16_t vlan = ((*(p+SIZE_ETHERNET)&017)<<8) + *(p+SIZE_ETHERNET+1);
+        int vlan = ((*(p+SIZE_ETHERNET)&017)<<8) + *(p+SIZE_ETHERNET+1);
+        vlans[vlan][0]++;
+        vlans[vlan][1] = vlan;
 
-        if(highest_used_count == num_of_allocated_counts)
-        {
-            vlan_count_t *newcounts = (vlan_count_t *) realloc(counts, 
-                    num_of_allocated_counts*sizeof(vlan_count_t) +
-                    ALLOC_COUNT*sizeof(vlan_count_t));
-
-            if(newcounts != NULL)
-            {
-                counts = newcounts;
-                num_of_allocated_counts += ALLOC_COUNT;
-            }
-        }
-        
-        uint16_t i;
-        for(i = 0; i <= highest_used_count; i++)
-        {
-            vlan_count_t *cur = &counts[i];
-            if(i == highest_used_count)
-            {
-                // not found so far, append it...
-                cur->vlan_id = vlan;
-                cur->count = 1;
-                highest_used_count++;
-                break;
-            }
-            else if(cur->vlan_id == vlan)
-            {
-                // current entry matched vlan-id
-                cur->count++;
-                break;
-            }
-        }
-
-        if(verbose)
-        {
+        if(verbose) {
             printf("Got packet, VLAN: %d\n", vlan);
         }
-    }
-    else
-    {   
+    } else {   
         untagged_count++;
-        if(verbose)
-        {
+        if(verbose) {
            printf("Got untagged packet!\n");
         }
     }
@@ -142,51 +102,53 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *p)
 
 static int compare_counts(const void *c1, const void *c2)
 {
-    const vlan_count_t *cc1 = (vlan_count_t*)(c1);
-    const vlan_count_t *cc2 = (vlan_count_t*)(c2);
-   
-    int retval; 
-    if(sort_by_count)
-    {
-        retval = cc1->count - cc2->count;
-    }
-    else
-    {
-        retval = cc1->vlan_id - cc2->vlan_id;
-    }
+    const vlan_entry_t *e1 = (vlan_entry_t*) c1;
+    const vlan_entry_t *e2 = (vlan_entry_t*) c2;
 
-    if(reverse_order)
-    {
-        return retval*-1;
+    if(reverse_order) {
+        return (e1->count - e2->count)*-1;
+    } else {
+        return e1->count - e2->count;
     }
-
-    return retval;
 }
 
 void print_totals()
 {
     printf("\n+---------------------------------------------+\n");
 
-    if(highest_used_count > 0)
-    {
-        qsort(counts, highest_used_count, sizeof(vlan_count_t),
-                compare_counts);
-
-        uint16_t i;
-        for(i = 0; i < highest_used_count; i++)
-        {
-            vlan_count_t *cur = &counts[i];
-            printf("| VLAN %4d: %20lld pkts.       |\n",
-                     cur->vlan_id, cur->count);
-        } 
-    }
-    else
-    {
+    int total_vlans = 0;
+    if(tagged_count > 0) {
+        int i,j;
+        if(sort_by_count) {
+            qsort(vlans, NUM_VLANS, sizeof(count_t)*2, compare_counts);
+            for(i = 0; i < NUM_VLANS; i++) {
+                if(vlans[i][0] > 0) {
+                    total_vlans++;
+                    printf("| VLAN %5lld: %20lld pkts.      |\n",
+                             vlans[i][1], vlans[i][0]);
+                }
+            }
+        } else {
+            for(i = 0; i < NUM_VLANS; i++) {
+                if(reverse_order) {
+                    j = NUM_VLANS-i;
+                } else {
+                    j = i;
+                }
+                
+                if(vlans[j][0] > 0) {
+                    total_vlans++;
+                    printf("| VLAN %5lld: %20lld pkts.      |\n",
+                             vlans[j][1], vlans[j][0]);
+                }
+            } 
+        }
+    } else {
         printf("| No tagged packets received!                 |\n");
     }
 
     printf("+---------------------------------------------+\n");
-    printf("| Total VLANs:           %20d |\n", highest_used_count);
+    printf("| Total VLANs:           %20d |\n", total_vlans);
     printf("| Total pkts:            %20lld |\n", total_count);
     printf("| Total tagged pkts:     %20lld |\n", tagged_count);
     printf("| Total untagged pkts:   %20lld |\n", untagged_count);
@@ -212,9 +174,8 @@ int main(int argc, char *argv[])
     printf("Copyright (c) 2009, Pascal Mainini\n");
     printf("All rights reserved.\n\n");
 
-    while ((c = getopt (argc, argv, "hvocr:i:")) != -1)
-        switch (c)
-        {
+    while ((c = getopt (argc, argv, "hvocr:i:")) != -1) {
+        switch (c) {
             case 'h':
                 PRINTHELP();
                 exit(EXIT_SUCCESS);
@@ -229,8 +190,7 @@ int main(int argc, char *argv[])
                 break;
             case 'r':
             case 'i':
-                if(mode != 'u')
-                {
+                if(mode != 'u') {
                     fprintf(stderr, "-i and -r can't be used together!\n");
                     abort();
                 }
@@ -238,110 +198,83 @@ int main(int argc, char *argv[])
                 source = optarg;
                 break;
            case '?':
-                if (optopt == 'i')
-                {
+                if (optopt == 'i') {
                     fprintf (stderr, 
                             "An interface must be specified with -i.\n");
-                }
-                else if (optopt == 'r')
-                {
+                } else if (optopt == 'r') {
                     fprintf (stderr, 
                             "A pcap-file must be specified with -r.\n");
-                }
-                else
-                {
+                } else {
                     PRINTHELP();
                     abort();
                 }
             default:
                 abort();
         }
+    }
 
-    if(mode == 'r')
-    {
+    if(mode == 'r') {
         handle = pcap_open_offline(source, errbuf);
-        if (handle == NULL) 
-        {
+        if (handle == NULL) {
             fprintf(stderr, "Error opening file %s: %s\n", source, errbuf);
             abort();
         }
-    }
-    else if(mode == 'i')
-    {
+    } else if(mode == 'i') {
         handle = pcap_open_live(source, BUFSIZ, 1, 1000, errbuf);
-        if (handle == NULL) 
-        {
+        if (handle == NULL) {
             fprintf(stderr, "Couldn't open device %s: %s\n", source, errbuf);
             abort();
         }
-    }
-    else
-    {
+    } else {
         PRINTHELP();
         exit(EXIT_SUCCESS);
     }
 
-    if(pcap_datalink(handle) != DLT_EN10MB)
-    {
+    if(pcap_datalink(handle) != DLT_EN10MB) {
         fprintf(stderr, "Unsuported link-type!\n");
         pcap_close(handle);
         abort();
     }
 
-    if(pcap_setnonblock(handle, 1, errbuf) != 0)
-    {
+    if(pcap_setnonblock(handle, 1, errbuf) != 0) {
         fprintf(stderr, "Couldn't put handle in non-blocking mode: %s\n", 
             errbuf);
         pcap_close(handle);
         abort();
     }
 
-    if (signal (SIGINT, interrupted) == SIG_IGN)
-    {
+    if (signal (SIGINT, interrupted) == SIG_IGN) {
         signal (SIGINT, SIG_IGN);
     }
-    if (signal (SIGHUP, interrupted) == SIG_IGN)
-    {
+    if (signal (SIGHUP, interrupted) == SIG_IGN) {
         signal (SIGHUP, SIG_IGN);
     }
-    if (signal (SIGTERM, interrupted) == SIG_IGN)
-    {
+    if (signal (SIGTERM, interrupted) == SIG_IGN) {
         signal (SIGTERM, SIG_IGN);
     }
 
-    uint8_t up = 1;
+    memset(&vlans, 0, NUM_VLANS*sizeof(count_t)*2);
+
+    char up = 1;
     int result;
-    while(up)
-    {
-        if((result = pcap_dispatch(handle, -1, got_packet, NULL)) > 0)
-        {
+    while(up) {
+        if((result = pcap_dispatch(handle, -1, got_packet, NULL)) > 0) {
             usleep(100);
-        }
-        else if(result == 0 && mode == 'i')
-        {
+        } else if(result == 0 && mode == 'i') {
             usleep(100);
-        }
-        else
-        {
+        } else {
             up = 0;
         }
     }
 
-    if(result == -1)
-    {
+    if(result == -1) {
         fprintf(stderr, "Error occured: %s\n", errbuf);
-    }
-    else if(result == -2)
-    {
+    } else if(result == -2) {
         printf("\nAborted.\n");
     }
 
     pcap_close(handle);
     print_totals();
-    if(num_of_allocated_counts > 0)
-    {
-        free(counts);
-    }
 
     exit(EXIT_SUCCESS);
 }
