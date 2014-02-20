0bc72ea40b22a3ab4ea10ef5d1b04bfe5448ba06
Pascal Mainini <pascal@mainini.ch>
2014-02-20 23:35:38 +0100

    added uri

--- vlandump-alloc.c
+++ vlandump-alloc.c
@@ -1,6 +1,6 @@
 /* 
 Small utility for capturing network traffic and listing found VLAN-ids.
-Copyright (C) 2009, Pascal Mainini
+Copyright (C) 2009, Pascal Mainini <http://mainini.ch>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
